8709cd8255834aa6e7a432640b34cb4510757fa7
Ben Stanfield <bnstnfld@gmail.com>
2020-08-31 12:41:04 -0700

    logging get-boards.js

--- endpoints/get-boards.js
+++ endpoints/get-boards.js
@@ -2,7 +2,10 @@ const fs = require('fs');
 
 exports.getBoards = async (req, res) => {
   const rawdata = fs.readFileSync('boards.json');
+  console.log('raw data: ', rawdata);
+  console.log('parsing json...');
   const boards = JSON.parse(rawdata);
+  console.log('boards: ', boards);
 
   res.status(200).send(boards);
 }
